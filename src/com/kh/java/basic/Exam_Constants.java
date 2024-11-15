1f3bd5744cc533618ccacac56dcf98db372706d7
user1 <user1@DESKTOP-NC04KRC>
2024-11-15 11:52:12 +0900

    [ADD] 24/11/15 변수와 자료형 그리고 상수
    1. 변수는 변하는 수이다. 리터럴을 저장하고 이름표의 역할을 한다.
    2. 자료형은 리터럴에 따라 다르다. 정수의 경우 자료형에 따라 저장할 수 있는 최대값이 다르다.
    3. 상수는 변하지 않는 수이다. final 키워드를 이용하여 재할당을 막아준다. PI의값을 저장할때 사용한다.

---
+++ src/com/kh/java/basic/Exam_Constants.java
@@ -0,0 +1,14 @@
+package com.kh.java.basic;
+
+public class Exam_Constants {
+	public static void main(String [] args) {
+		// 상수는 변하지 않는 수 
+		// 선언시 대문자로 표시
+		double PI = 3.14;
+		
+		//원의 넓이 공식 : 3.14 * r * r
+		double radius = 5.7;
+		double circleArea;
+		circleArea = 3.14 * radius * radius;
+	}
+}
